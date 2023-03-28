c5b735dd744434e2eef3f94972b98305f78443df
sophie mattacks <s.mattacks@hotmail.co.uk>
2023-03-28 11:19:26 +0100

    Set up to-do list app, with working props.

---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+const Todo = () => {
+  return (
+    <div className='Todo'>
+      <p>Go to school</p>
+      <div>
+        <FontAwesomeIcon icon={faPenToSquare} />
+        <FontAwesomeIcon icon={faTrash} />
+      </div>
+    </div>
+  )
+}
+
+export default Todo
